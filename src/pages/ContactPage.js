fd171a2b502f6fd3abaadc52964be9b0050a6105
Ovidiu Mura <ovidiu.mura@yahoo.com>
2024-01-01 16:21:04 -0800

    adding summary and contact pages

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,13 +1,24 @@
 
 import React from "react";
 
+import './css/ContactPage.css';
+
 export default function ContactPage() {
 
     return (
         <>
-        <h1>Hey from ContactPage</h1>
-        <div>
-            Contact me at ovidiu.mura@yahoo.com for projects.
+        <h1 className="title">Contact</h1>
+        <div className="content">
+            {/* Email: <span className="email">ovidiu.mura@yahoo.com</span> */}
+            <table className="styled-table">
+                    {/* <thead>
+                        <tr><th>Email</th><th>Points</th></tr>
+                    </thead> */}
+                    <tbody>
+                        <tr><td>Email:</td><td><span className="email">ovidiu.mura@yahoo.com</span></td></tr>
+                        <tr className="active-row"><td>Phone:</td><td>(971) 990-9319</td></tr>
+                    </tbody>
+            </table>
         </div>
         </>
     );
